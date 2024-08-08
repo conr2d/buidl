3b1867c076ffd5569f7509f9843cb6322eb90f37
Jeeyong Um <conr2d@proton.me>
2024-08-08 15:37:38 +0900

    feat: Make deriving substrate traits configurable (#5)

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -45,14 +45,39 @@ mod utils;
 /// - [`Clone`]
 /// - [`PartialEq`], [`Eq`], [`PartialOrd`], [`Ord`]
 /// - [`From`], [`TryFrom`], [`AsRef`], [`AsMut`]
-/// - [`Hash`](core::hash::Hash)
-/// - [`Deref`](core::ops::Deref)
+/// - [`Hash`]
+/// - [`Deref`], [`DerefMut`]
+///
+/// [`Hash`]: core::hash::Hash
+/// [`Deref`]: core::ops::Deref
+/// [`DerefMut`]: core::ops::DerefMut
 ///
 /// ### Polkadot SDK traits (Optional)
-/// - `Encode`, `EncodeLike`, `Decode`, `MaxEncodedLen`
-/// - `TypeInfo`
-/// - `ByteArray`, `UncheckedFrom`, `FromEntropy`
-/// - `PassBy`, `PassByInner`
+///
+/// Polkadot SDK traits are derived when the `substrate` attribute is specified.
+///
+/// ```
+/// # use buidl_derive::FixedBytes;
+/// # use core::marker::PhantomData;
+/// #[derive(FixedBytes)]
+/// #[buidl(substrate)]
+/// struct CryptoBytes<const N: usize, T = ()>([u8; N], PhantomData<fn() -> T>);
+/// ```
+///
+/// - [`ByteArray`], [`UncheckedFrom`], [`FromEntropy`]
+/// - [`PassBy`], [`PassByInner`]
+/// - [`Encode`], [`EncodeLike`], [`Decode`], `MaxEncodedLen`
+/// - [`TypeInfo`]
+///
+/// [`ByteArray`]: https://docs.rs/sp-core/32.0.0/sp_core/crypto/trait.ByteArray.html
+/// [`UncheckedFrom`]: https://docs.rs/sp-core/32.0.0/sp_core/crypto/trait.UncheckedFrom.html
+/// [`FromEntropy`]: https://docs.rs/sp-core/32.0.0/sp_core/crypto/trait.FromEntropy.html
+/// [`PassBy`]: https://docs.rs/sp-runtime-interface/27.0.0/sp_runtime_interface/pass_by/trait.PassBy.html
+/// [`PassByInner`]: https://docs.rs/sp-runtime-interface/27.0.0/sp_runtime_interface/pass_by/trait.PassByInner.html
+/// [`Decode`]: https://docs.rs/parity-scale-codec/3.6.12/parity_scale_codec/trait.Decode.html
+/// [`Encode`]: https://docs.rs/parity-scale-codec/3.6.12/parity_scale_codec/trait.Encode.html
+/// [`EncodeLike`]: https://docs.rs/parity-scale-codec/3.6.12/parity_scale_codec/trait.EncodeLike.html
+/// [`TypeInfo`]: https://docs.rs/scale-info/2.11.3/scale_info/trait.TypeInfo.html
 #[proc_macro_derive(FixedBytes, attributes(buidl))]
 pub fn derive_fixed_bytes(input: TokenStream) -> TokenStream {
 	let input = parse_macro_input!(input as DeriveInput);
