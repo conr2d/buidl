d5f991075cd926be6feae676ca24faf60d503b0c
Jeeyong Um <conr2d@proton.me>
2025-01-18 20:41:14 +0900

    style: Apply rustfmt default formatting

--- derive/src/fixed_bytes.rs
+++ derive/src/fixed_bytes.rs
@@ -13,15 +13,15 @@ use quote::{quote, ToTokens};
 use syn::{Data, DataStruct, DeriveInput, Expr, Field, Fields, Ident, Index, Type};
 
 mod kw {
-	pub const DERIVE: &str = "derive";
-	pub const SKIP_DERIVE: &str = "skip_derive";
+    pub const DERIVE: &str = "derive";
+    pub const SKIP_DERIVE: &str = "skip_derive";
 
-	pub const CLONE: &str = "Clone";
-	pub const CODEC: &str = "Codec";
-	pub const PASS_BY: &str = "PassBy";
-	pub const SCALE: &str = "Scale";
-	pub const SUBSTRATE: &str = "Substrate";
-	pub const TYPE_INFO: &str = "TypeInfo";
+    pub const CLONE: &str = "Clone";
+    pub const CODEC: &str = "Codec";
+    pub const PASS_BY: &str = "PassBy";
+    pub const SCALE: &str = "Scale";
+    pub const SUBSTRATE: &str = "Substrate";
+    pub const TYPE_INFO: &str = "TypeInfo";
 }
 
 pub const SUPPORTED_ATTRIBUTES: &[&str] = &[kw::DERIVE, kw::SKIP_DERIVE];
@@ -29,68 +29,72 @@ pub const SUPPORTED_DERIVES: &[&str] = &[kw::SCALE, kw::SUBSTRATE];
 pub const SUPPORTED_SKIP_DERIVES: &[&str] = &[kw::CLONE, kw::CODEC, kw::PASS_BY, kw::TYPE_INFO];
 
 trait Contains<T> {
-	fn contains<U>(&self, value: U) -> bool
-	where
-		T: PartialEq<U>;
+    fn contains<U>(&self, value: U) -> bool
+    where
+        T: PartialEq<U>;
 }
 impl<T> Contains<T> for Vec<T> {
-	fn contains<U>(&self, value: U) -> bool
-	where
-		T: PartialEq<U>,
-	{
-		self.iter().any(|item| item == &value)
-	}
+    fn contains<U>(&self, value: U) -> bool
+    where
+        T: PartialEq<U>,
+    {
+        self.iter().any(|item| item == &value)
+    }
 }
 
 pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
-	let ty = &input.ident;
-	let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
-	let attrs = utils::parse_top_attributes(&input);
-	let attrs_str = attrs
-		.iter()
-		.filter_map(|attr| attr.path().get_ident().map(|ident| ident.to_string()))
-		.collect::<Vec<_>>();
-	check_attributes("buidl", &attrs_str, SUPPORTED_ATTRIBUTES);
+    let ty = &input.ident;
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let attrs = utils::parse_top_attributes(&input);
+    let attrs_str = attrs
+        .iter()
+        .filter_map(|attr| attr.path().get_ident().map(|ident| ident.to_string()))
+        .collect::<Vec<_>>();
+    check_attributes("buidl", &attrs_str, SUPPORTED_ATTRIBUTES);
 
-	let fields = check_struct_fields(&input.data);
-	let field_names = struct_field_names(fields.clone());
+    let fields = check_struct_fields(&input.data);
+    let field_names = struct_field_names(fields.clone());
 
-	// first field should be a u8 array.
-	let inner = &fields[0].ty;
-	let size = check_array_size(inner);
-	let data = field_names[0].clone();
+    // first field should be a u8 array.
+    let inner = &fields[0].ty;
+    let size = check_array_size(inner);
+    let data = field_names[0].clone();
 
-	// build an initializer for the remaining fields.
-	let remaining = (fields.len() > 1).then(|| {
-		fields.into_iter().enumerate().skip(1).fold(quote!(), |acc, (i, f)| {
-			let ident = field_names[i].clone();
-			match utils::field_default_value(f) {
-				Some(value) => quote! { #acc #ident: #value, },
-				None => quote! { #acc #ident: Default::default(), },
-			}
-		})
-	});
+    // build an initializer for the remaining fields.
+    let remaining = (fields.len() > 1).then(|| {
+        fields
+            .into_iter()
+            .enumerate()
+            .skip(1)
+            .fold(quote!(), |acc, (i, f)| {
+                let ident = field_names[i].clone();
+                match utils::field_default_value(f) {
+                    Some(value) => quote! { #acc #ident: #value, },
+                    None => quote! { #acc #ident: Default::default(), },
+                }
+            })
+    });
 
-	let derive = utils::find_list_strings(&attrs, "derive");
-	check_attributes("derive", &derive, SUPPORTED_DERIVES);
+    let derive = utils::find_list_strings(&attrs, "derive");
+    check_attributes("derive", &derive, SUPPORTED_DERIVES);
 
-	let skip_derive = utils::find_list_strings(&attrs, "skip_derive");
-	check_attributes("skip_derive", &skip_derive, SUPPORTED_SKIP_DERIVES);
+    let skip_derive = utils::find_list_strings(&attrs, "skip_derive");
+    check_attributes("skip_derive", &skip_derive, SUPPORTED_SKIP_DERIVES);
 
-	let mut output = Vec::new();
+    let mut output = Vec::new();
 
-	if !skip_derive.contains(kw::CLONE) {
-		output.push(quote! {
-			#[automatically_derived]
-			impl #impl_generics Clone for #ty #ty_generics #where_clause {
-				fn clone(&self) -> Self {
-					Self { #( #field_names: self.#field_names.clone(), )* }
-				}
-			}
-		});
-	}
+    if !skip_derive.contains(kw::CLONE) {
+        output.push(quote! {
+            #[automatically_derived]
+            impl #impl_generics Clone for #ty #ty_generics #where_clause {
+                fn clone(&self) -> Self {
+                    Self { #( #field_names: self.#field_names.clone(), )* }
+                }
+            }
+        });
+    }
 
-	output.push(quote! {
+    output.push(quote! {
 		#[automatically_derived]
 		impl #impl_generics PartialEq for #ty #ty_generics #where_clause {
 			fn eq(&self, other: &Self) -> bool {
@@ -176,10 +180,10 @@ pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
 		}
 	});
 
-	if derive.contains(kw::SUBSTRATE) {
-		let sp_core = utils::crate_access("sp-core");
+    if derive.contains(kw::SUBSTRATE) {
+        let sp_core = utils::crate_access("sp-core");
 
-		output.push(quote! {
+        output.push(quote! {
 			#[automatically_derived]
 			impl #impl_generics ::#sp_core::crypto::ByteArray for #ty #ty_generics #where_clause {
 				const LEN: usize = #size;
@@ -193,10 +197,10 @@ pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
 			}
 		});
 
-		if !skip_derive.contains(kw::CODEC) {
-			let parity_scale_codec = utils::crate_access("parity-scale-codec");
+        if !skip_derive.contains(kw::CODEC) {
+            let parity_scale_codec = utils::crate_access("parity-scale-codec");
 
-			output.push(quote! {
+            output.push(quote! {
 				#[automatically_derived]
 				impl #impl_generics ::#sp_core::crypto::FromEntropy for #ty #ty_generics #where_clause {
 					fn from_entropy(input: &mut impl ::#parity_scale_codec::Input) -> Result<Self, ::#parity_scale_codec::Error> {
@@ -206,12 +210,12 @@ pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
 					}
 				}
 			});
-		}
+        }
 
-		if !skip_derive.contains(kw::PASS_BY) {
-			let sp_runtime_interface = utils::crate_access("sp-runtime-interface");
+        if !skip_derive.contains(kw::PASS_BY) {
+            let sp_runtime_interface = utils::crate_access("sp-runtime-interface");
 
-			output.push(quote! {
+            output.push(quote! {
 				#[automatically_derived]
 				impl #impl_generics ::#sp_runtime_interface::pass_by::PassByInner for #ty #ty_generics #where_clause {
 					type Inner = #inner;
@@ -234,14 +238,14 @@ pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
 					type PassBy = ::#sp_runtime_interface::pass_by::Inner<Self, #inner>;
 				}
 			});
-		}
-	}
+        }
+    }
 
-	if derive.contains(kw::SUBSTRATE) || derive.contains(kw::SCALE) {
-		if !skip_derive.contains(kw::CODEC) {
-			let parity_scale_codec = utils::crate_access("parity-scale-codec");
+    if derive.contains(kw::SUBSTRATE) || derive.contains(kw::SCALE) {
+        if !skip_derive.contains(kw::CODEC) {
+            let parity_scale_codec = utils::crate_access("parity-scale-codec");
 
-			output.push(quote! {
+            output.push(quote! {
 				#[automatically_derived]
 				impl #impl_generics ::#parity_scale_codec::Encode for #ty #ty_generics #where_clause {
 					fn size_hint(&self) -> usize {
@@ -270,87 +274,91 @@ pub fn expand_fixed_bytes(input: DeriveInput) -> TokenStream {
 					}
 				}
 			});
-		}
+        }
 
-		if !skip_derive.contains(kw::TYPE_INFO) {
-			let scale_info = utils::crate_access("scale-info");
+        if !skip_derive.contains(kw::TYPE_INFO) {
+            let scale_info = utils::crate_access("scale-info");
 
-			output.push(quote! {
-				#[automatically_derived]
-				impl #impl_generics ::#scale_info::TypeInfo for #ty #ty_generics #where_clause {
-					type Identity = #inner;
+            output.push(quote! {
+                #[automatically_derived]
+                impl #impl_generics ::#scale_info::TypeInfo for #ty #ty_generics #where_clause {
+                    type Identity = #inner;
 
-					fn type_info() -> ::#scale_info::Type {
-						Self::Identity::type_info()
-					}
-				}
-			});
-		}
-	}
+                    fn type_info() -> ::#scale_info::Type {
+                        Self::Identity::type_info()
+                    }
+                }
+            });
+        }
+    }
 
-	quote! {
-		#(#output)*
-	}
+    quote! {
+        #(#output)*
+    }
 }
 
 fn check_struct_fields(data: &Data) -> Vec<&Field> {
-	match data {
-		Data::Struct(DataStruct { fields, .. }) => match fields {
-			Fields::Named(fields) => fields.named.iter().collect(),
-			Fields::Unnamed(fields) => fields.unnamed.iter().collect(),
-			Fields::Unit => panic!("no fields"),
-		},
-		_ => panic!("`struct` expected"),
-	}
+    match data {
+        Data::Struct(DataStruct { fields, .. }) => match fields {
+            Fields::Named(fields) => fields.named.iter().collect(),
+            Fields::Unnamed(fields) => fields.unnamed.iter().collect(),
+            Fields::Unit => panic!("no fields"),
+        },
+        _ => panic!("`struct` expected"),
+    }
 }
 
 fn check_attributes(attr: &str, items: &[String], supported: &[&str]) {
-	let unsupported = items
-		.iter()
-		.filter(|item| !supported.contains(&item.as_str()))
-		.collect::<Vec<_>>();
-	if !unsupported.is_empty() {
-		panic!("`{}` doesn't support {:?}", attr, unsupported);
-	}
+    let unsupported = items
+        .iter()
+        .filter(|item| !supported.contains(&item.as_str()))
+        .collect::<Vec<_>>();
+    if !unsupported.is_empty() {
+        panic!("`{}` doesn't support {:?}", attr, unsupported);
+    }
 }
 
 #[derive(Clone)]
 enum FieldName {
-	Named(Ident),
-	Unnamed(Index),
+    Named(Ident),
+    Unnamed(Index),
 }
 
 impl ToTokens for FieldName {
-	fn to_tokens(&self, tokens: &mut TokenStream) {
-		match self {
-			FieldName::Named(ident) => ident.to_tokens(tokens),
-			FieldName::Unnamed(index) => index.to_tokens(tokens),
-		}
-	}
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            FieldName::Named(ident) => ident.to_tokens(tokens),
+            FieldName::Unnamed(index) => index.to_tokens(tokens),
+        }
+    }
 }
 
 impl From<(usize, &Field)> for FieldName {
-	fn from((index, field): (usize, &Field)) -> Self {
-		match field.ident {
-			Some(ref ident) => FieldName::Named(ident.clone()),
-			None => FieldName::Unnamed(Index::from(index)),
-		}
-	}
+    fn from((index, field): (usize, &Field)) -> Self {
+        match field.ident {
+            Some(ref ident) => FieldName::Named(ident.clone()),
+            None => FieldName::Unnamed(Index::from(index)),
+        }
+    }
 }
 
 fn struct_field_names(fields: Vec<&Field>) -> Vec<FieldName> {
-	fields.into_iter().enumerate().map(FieldName::from).collect()
+    fields
+        .into_iter()
+        .enumerate()
+        .map(FieldName::from)
+        .collect()
 }
 
 fn check_array_size(ty: &Type) -> Expr {
-	if let Type::Array(arr) = ty {
-		if let Type::Path(path) = &*arr.elem {
-			if let Some(segment) = path.path.segments.first() {
-				if segment.ident == "u8" {
-					return arr.len.clone()
-				}
-			}
-		}
-	}
-	panic!("expected u8 array");
+    if let Type::Array(arr) = ty {
+        if let Type::Path(path) = &*arr.elem {
+            if let Some(segment) = path.path.segments.first() {
+                if segment.ident == "u8" {
+                    return arr.len.clone();
+                }
+            }
+        }
+    }
+    panic!("expected u8 array");
 }
--- derive/src/utils.rs
+++ derive/src/utils.rs
@@ -10,73 +10,85 @@
 use proc_macro2::Span;
 use proc_macro_crate::{crate_name, FoundCrate};
 use syn::{
-	parse::Parse, punctuated::Punctuated, AttrStyle, Attribute, DeriveInput, Expr, Field, Ident,
-	Meta, Token,
+    parse::Parse, punctuated::Punctuated, AttrStyle, Attribute, DeriveInput, Expr, Field, Ident,
+    Meta, Token,
 };
 
 pub fn field_default_value(field: &Field) -> Option<Expr> {
-	let outer_attrs = field.attrs.iter().filter(|attr| matches!(attr.style, AttrStyle::Outer));
+    let outer_attrs = field
+        .attrs
+        .iter()
+        .filter(|attr| matches!(attr.style, AttrStyle::Outer));
 
-	find_meta_item("buidl", outer_attrs, |meta| {
-		if let Meta::NameValue(nv) = meta {
-			if nv.path.is_ident("default") {
-				return Some(nv.value)
-			}
-		}
+    find_meta_item("buidl", outer_attrs, |meta| {
+        if let Meta::NameValue(nv) = meta {
+            if nv.path.is_ident("default") {
+                return Some(nv.value);
+            }
+        }
 
-		None
-	})
+        None
+    })
 }
 
 pub fn crate_access(name: &str) -> Ident {
-	let name = match crate_name(name) {
-		Ok(FoundCrate::Itself) => name.to_string().replace("-", "_"),
-		Ok(FoundCrate::Name(name)) => name,
-		Err(_) => panic!("should have `{}` in dependencies", name),
-	};
+    let name = match crate_name(name) {
+        Ok(FoundCrate::Itself) => name.to_string().replace("-", "_"),
+        Ok(FoundCrate::Name(name)) => name,
+        Err(_) => panic!("should have `{}` in dependencies", name),
+    };
 
-	Ident::new(&name, Span::call_site())
+    Ident::new(&name, Span::call_site())
 }
 
 pub fn parse_top_attributes(input: &DeriveInput) -> Vec<Meta> {
-	let mut outer_attrs = input.attrs.iter().filter(|attr| matches!(attr.style, AttrStyle::Outer));
+    let mut outer_attrs = input
+        .attrs
+        .iter()
+        .filter(|attr| matches!(attr.style, AttrStyle::Outer));
 
-	outer_attrs
-		.find_map(|attr| {
-			attr.path().is_ident("buidl").then(|| {
-				let nested_meta =
-					attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated).unwrap();
-				nested_meta.into_iter().collect()
-			})
-		})
-		.unwrap_or_default()
+    outer_attrs
+        .find_map(|attr| {
+            attr.path().is_ident("buidl").then(|| {
+                let nested_meta = attr
+                    .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
+                    .unwrap();
+                nested_meta.into_iter().collect()
+            })
+        })
+        .unwrap_or_default()
 }
 
 pub fn parse_list_items(meta: &Meta) -> Vec<Meta> {
-	match meta {
-		Meta::List(list) => list
-			.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
-			.unwrap()
-			.into_iter()
-			.collect(),
-		_ => Vec::new(),
-	}
+    match meta {
+        Meta::List(list) => list
+            .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
+            .unwrap()
+            .into_iter()
+            .collect(),
+        _ => Vec::new(),
+    }
 }
 
 pub fn find_list_strings<'a>(attrs: impl IntoIterator<Item = &'a Meta>, kind: &str) -> Vec<String> {
-	attrs
-		.into_iter()
-		.filter(|attr| attr.path().is_ident(kind))
-		.flat_map(parse_list_items)
-		.filter_map(|meta| meta.path().get_ident().map(|ident| ident.to_string()))
-		.collect()
+    attrs
+        .into_iter()
+        .filter(|attr| attr.path().is_ident(kind))
+        .flat_map(parse_list_items)
+        .filter_map(|meta| meta.path().get_ident().map(|ident| ident.to_string()))
+        .collect()
 }
 
 fn find_meta_item<'a, F, R, I, M>(kind: &str, mut itr: I, mut pred: F) -> Option<R>
 where
-	F: FnMut(M) -> Option<R> + Clone,
-	I: Iterator<Item = &'a Attribute>,
-	M: Parse,
+    F: FnMut(M) -> Option<R> + Clone,
+    I: Iterator<Item = &'a Attribute>,
+    M: Parse,
 {
-	itr.find_map(|attr| attr.path().is_ident(kind).then(|| pred(attr.parse_args().ok()?)).flatten())
+    itr.find_map(|attr| {
+        attr.path()
+            .is_ident(kind)
+            .then(|| pred(attr.parse_args().ok()?))
+            .flatten()
+    })
 }
